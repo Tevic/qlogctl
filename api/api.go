eb70d42f1321ec5a2fba23eef2b0ce2f25555ee8
Sxci <sxci@foxmail.com>
2017-04-26 17:34:20 +0800

    可设置用户别名，通过别名登录

--- api/api.go
+++ api/api.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -27,12 +28,13 @@ type CtlArg struct {
 	Sort   string
 	Start  string
 	End    string
-	fields []string
+	fields []logdb.RepoSchemaEntry
 	Head   int
 	Debug  bool
 }
 
 type logCtlInfo struct {
+	User     string                  `json:"user"`
 	Range    int                     `json:"timeRange"`
 	RepoName string                  `json:"repoName"`
 	Ak       string                  `json:"ak"`
@@ -42,6 +44,7 @@ type logCtlInfo struct {
 }
 
 var _info *logCtlInfo
+var _logCtlCtx *logCtlCtx
 var _client logdb.LogdbAPI
 
 const _tempFile = ".qn_logdb_ctl_profile"
@@ -86,7 +89,7 @@ func doQuery(query *string, arg *CtlArg, info *logCtlInfo, sort *string, from in
 	}
 
 	if arg.Debug {
-		log.Println(queryInput)
+		log.Println("xxxxx", queryInput)
 		log.Println()
 	}
 
@@ -158,7 +161,7 @@ func buildQuery(query *string, arg *CtlArg, defaultRange int, dateField *string)
 
 func showLogs(logs *logdb.QueryLogOutput, arg *CtlArg, info *logCtlInfo, from int) {
 	if arg.fields == nil || len(arg.fields) == 0 {
-		arg.fields = getShowFields(arg.Field, info)
+		arg.fields = getShowFields(arg.Field, info.Repo)
 	}
 
 	for i, v := range logs.Data {
@@ -166,22 +169,23 @@ func showLogs(logs *logdb.QueryLogOutput, arg *CtlArg, info *logCtlInfo, from in
 	}
 }
 
-func getLogStr(log *map[string]interface{}, fields *[]string, split string, verbose bool) string {
+func getLogStr(log *map[string]interface{}, fields *[]logdb.RepoSchemaEntry, split string, verbose bool) string {
 	values := []string{}
-	for _, filed := range *fields {
-		v := (*log)[filed]
+	for _, entry := range *fields {
+		field := entry.Key
+		v := (*log)[field]
 		// "valtype":"long"  被转换为 float64 ,显示不友好
-		switch v.(type) {
-		case float64, float32:
+		switch entry.ValueType {
+		case "long":
 			if verbose {
-				values = append(values, fmt.Sprintf(warpRed("%11s:")+"\t%.0f", filed, v))
+				values = append(values, fmt.Sprintf(warpRed("%11s:")+"\t%.0f", field, v))
 			} else {
 				values = append(values, fmt.Sprintf("%.0f", v))
 			}
 			break
 		default:
 			if verbose {
-				values = append(values, fmt.Sprintf(warpRed("%11s:")+"\t%v", filed, v))
+				values = append(values, fmt.Sprintf(warpRed("%11s:")+"\t%v", field, v))
 			} else {
 				values = append(values, fmt.Sprint(v))
 			}
@@ -191,19 +195,37 @@ func getLogStr(log *map[string]interface{}, fields *[]string, split string, verb
 	return strings.Join(values, split)
 }
 
-func getShowFields(fieldsStr string, info *logCtlInfo) []string {
-	fields := []string{}
+func getShowFields(fieldsStr string, repo *logdb.GetRepoOutput) []logdb.RepoSchemaEntry {
+	temp := []logdb.RepoSchemaEntry{}
+	for _, e := range repo.Schema {
+		temp = append(temp, e)
+	}
+	if fieldsStr == "*" {
+		return temp
+	}
+
+	fields := []logdb.RepoSchemaEntry{}
 	for _, v := range strings.Split(fieldsStr, ",") {
 		v = strings.TrimSpace(v)
 		if "*" == v {
-			for _, e := range info.Repo.Schema {
-				fields = append(fields, e.Key)
-			}
+			fields = append(fields, temp...)
 		} else {
-			fields = append(fields, v)
+			field := getField(temp, v)
+			if field != nil {
+				fields = append(fields, *field)
+			}
 		}
 	}
 	return fields
+}
+
+func getField(fields []logdb.RepoSchemaEntry, key string) *logdb.RepoSchemaEntry {
+	for _, v := range fields {
+		if v.Key == key {
+			return &v
+		}
+	}
+	return nil
 }
 
 // QueryHistogram query histogram
@@ -355,48 +377,31 @@ func parseReqid(reqid string) (unixNano int64, err error) {
 	return
 }
 
-// Login by ak and sk
-func Login(ak string, sk string) {
-	info := &logCtlInfo{}
-	info.Ak = ak
-	info.Sk = sk
-	info.Range = 5
-	storeInfo(info)
-}
-
 // SetRepo set current repo
-func SetRepo(repoName string) {
+func SetRepo(repoName string, refresh bool) {
 	buildLogCtlInfo()
-	if len(repoName) != 0 {
-		info := getInfoByName(repoName)
-		if len(info.RepoName) != 0 {
-			_info.RepoName = repoName
-			_info.Repo = info.Repo
-			go storeInfo(_info)
-		} else {
-			fmt.Printf("Set repo: %s failed, Nothing changed.\n", repoName)
+	if !refresh {
+		if len(repoName) == 0 || repoName == _info.RepoName {
+			showRepo(_info)
+			return
+		}
+		info := getCtlInfo(_logCtlCtx, _info.User, repoName)
+		if info != nil && info.Repo != nil {
+			showRepo(info)
+			storeInfo(info)
 			return
 		}
 	}
-	showRepo(_info)
-
-	if len(repoName) != 0 {
-		sample, err := doQuerySample(repoName, _info.Repo)
-		if err != nil {
-			log.Println(err)
-			_info.Log = nil
-		} else {
-			_info.Log = sample
-		}
+	repo, sample, err := getNewInfoByName(repoName)
+	if err != nil {
+		log.Println(err)
+		return
 	}
-
-	// 显示样例
-	if _info.Log != nil {
-		fmt.Println("Sample: ")
-		fields := getShowFields("*", _info)
-		fmt.Printf("%s\n", getLogStr(_info.Log, &fields, "\n", true))
-	}
+	_info.RepoName = repoName
+	_info.Repo = repo
+	_info.Log = sample
 	storeInfo(_info)
+	showRepo(_info)
 }
 
 // QuerySample sample
@@ -413,9 +418,14 @@ func QuerySample() {
 	}
 	// 显示样例
 	if _info.Log != nil {
-		fields := getShowFields("*", _info)
+		fields := getShowFields("*", _info.Repo)
 		fmt.Printf("%s\n", getLogStr(_info.Log, &fields, "\n", true))
 	}
+}
+
+// Clear the cache
+func Clear() {
+	os.Remove(userHomeDir() + "/" + _tempFile)
 }
 
 // SetTimeRange set range
@@ -469,32 +479,19 @@ func ListRepos(verbose bool) {
 	}
 }
 
-// ShowRepo show repo
-func ShowRepo(repoName string) {
-	showRepo(getInfoByName(repoName))
-}
-
-func getInfoByName(repoName string) (info *logCtlInfo) {
-	if len(repoName) == 0 {
-		buildLogCtlInfo()
-		return _info
-	}
-	err := buildClient()
+func getNewInfoByName(repoName string) (repo *logdb.GetRepoOutput, sample *map[string]interface{}, err error) {
+	err = buildClient()
 	if err != nil {
-		log.Println(err)
 		return
 	}
 
-	repo, err := _client.GetRepo(&logdb.GetRepoInput{RepoName: repoName})
+	repo, err = _client.GetRepo(&logdb.GetRepoInput{RepoName: repoName})
 	if err != nil {
-		log.Println(err)
 		return
 	}
+	sample, err = doQuerySample(repoName, repo)
 
-	return &logCtlInfo{
-		RepoName: repoName,
-		Repo:     repo,
-	}
+	return
 }
 
 func doQuerySample(repoName string, repo *logdb.GetRepoOutput) (log *map[string]interface{}, err error) {
@@ -519,13 +516,13 @@ func doQuerySample(repoName string, repo *logdb.GetRepoOutput) (log *map[string]
 func showRepo(info *logCtlInfo) {
 	repo := info.Repo
 	// 显示 Repo 信息
-	fmt.Printf("%11s: %s\n", "RepoName", info.RepoName)
+	fmt.Printf("\n%11s: %s\n", "RepoName", info.RepoName)
 	fmt.Printf("%11s: %s\n", "Region", repo.Region)
 	fmt.Printf("%11s: %s\n", "Retention", repo.Retention)
 	// 显示字段信息
-	fmt.Printf("Field: (%d)\n", len(repo.Schema))
+	fmt.Printf("\nField: (%d)\n", len(repo.Schema))
 	var dateField string
-	for _, e := range info.Repo.Schema {
+	for _, e := range repo.Schema {
 		if e.ValueType == "date" && len(dateField) == 0 {
 			dateField = e.Key
 			fmt.Printf(warpRed("%v\n"), e)
@@ -535,10 +532,88 @@ func showRepo(info *logCtlInfo) {
 	}
 	// 显示时间字段信息
 	fmt.Printf("%s，时间字段为： %s ，默认排序为： %s\n", warpRed(info.RepoName), warpRed(dateField), warpRed(dateField+":desc"))
+
+	// 显示样例
+	if info.Log != nil {
+		fmt.Println("\nSample: ")
+		fields := getShowFields("*", repo)
+		fmt.Printf("%s\n", getLogStr(info.Log, &fields, "\n", true))
+	}
 }
 
 func warpRed(s string) string {
 	return fmt.Sprintf("\033[0;31m%s\033[0m", s)
+}
+
+// Login by ak and sk
+func Login(ak string, sk string, alias string) {
+	if len(sk) != 0 && len(alias) != 0 {
+		info := buildUserContect(ak, sk, alias)
+		if info == _info && info.Repo != nil {
+			showRepo(_info)
+			return
+		}
+		if info != _info {
+			err := setCurrentUser(info)
+			if err != nil {
+				fmt.Println("设置 账号信息失败，请重试 ...")
+				return
+			}
+		}
+		ListRepos(false)
+		fmt.Println(warpRed("请设置 REPO ..."))
+		return
+	}
+	user := &alias
+	if len(ak) != 0 {
+		user = &ak
+	}
+	buildLogCtlInfo()
+	_info = getCtlInfo(_logCtlCtx, *user, "")
+	storeInfo(_info)
+	if _info.Repo != nil {
+		showRepo(_info)
+	} else {
+		ListRepos(false)
+		fmt.Println(warpRed("请设置 REPO ..."))
+	}
+}
+
+// UserList list user names
+func UserList() {
+	buildLogCtlInfo()
+	keys := make([]string, 0)
+	if _logCtlCtx == nil || _logCtlCtx.Data == nil {
+		fmt.Println("没有已设置的登录信息")
+		return
+	}
+	for k := range *_logCtlCtx.Data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for i, k := range keys {
+		fmt.Printf("%v: %v\n", i, k)
+	}
+}
+
+func buildUserContect(ak string, sk string, alias string) *logCtlInfo {
+	buildLogCtlInfo()
+	if _info != nil && _info.User == alias && _info.Ak == ak && _info.Sk == sk {
+		return _info
+	}
+	info := &logCtlInfo{}
+	info.User = alias
+	info.Ak = ak
+	info.Sk = sk
+	return info
+}
+
+func setCurrentUser(info *logCtlInfo) (err error) {
+	if _logCtlCtx == nil {
+		_logCtlCtx = &logCtlCtx{}
+	}
+	_logCtlCtx.Current = info.User
+	return storeInfo(info)
 }
 
 func userHomeDir() string {
@@ -552,26 +627,116 @@ func userHomeDir() string {
 	return os.Getenv("HOME")
 }
 
-func buildLogCtlInfo() (err error) {
-	if _info == nil {
-		data, err := ioutil.ReadFile(userHomeDir() + "/" + _tempFile)
-		if err != nil {
-			return err
-		}
-		_info = &logCtlInfo{}
-		json.Unmarshal(data, _info)
-	}
-	return
+type logCtlCtx struct {
+	Data    *map[string]*logCtlCtxData `json:"data"`
+	Current string                     `json:"current"`
 }
 
-func storeInfo(v *logCtlInfo) {
-	bytes, err := json.Marshal(v)
+type logCtlCtxData struct {
+	AK    string                         `json:"ak"`
+	SK    string                         `json:"sk"`
+	Repo  string                         `json:"repo"`
+	Range int                            `json:"range"`
+	Data  *map[string]*logCtlCtxRepoData `json:"data"`
+}
+
+type logCtlCtxRepoData struct {
+	Repo *logdb.GetRepoOutput    `json:"repo"`
+	Log  *map[string]interface{} `json:"log"`
+}
+
+func buildLogCtlInfo() error {
+	if _info == nil {
+		bytes, err := ioutil.ReadFile(userHomeDir() + "/" + _tempFile)
+		if err != nil {
+			log.Println("Opps....", err)
+			return err
+		}
+		if _logCtlCtx == nil {
+			_logCtlCtx = &logCtlCtx{}
+		}
+		err = json.Unmarshal(bytes, _logCtlCtx)
+		if err != nil {
+			log.Println("Opps....", err)
+			return err
+		}
+		_info = getCtlInfo(_logCtlCtx, _logCtlCtx.Current, "")
+	}
+	return nil
+}
+
+func getCtlInfo(ctx *logCtlCtx, user string, repoName string) *logCtlInfo {
+	if len(user) == 0 || ctx.Data == nil {
+		return nil
+	}
+	info := &logCtlInfo{}
+
+	userData := (*ctx.Data)[user]
+
+	info.User = user
+	info.Ak = userData.AK
+	info.Sk = userData.SK
+	info.Range = userData.Range
+	if info.Range < 1 {
+		info.Range = 5
+	}
+	if len(repoName) != 0 {
+		info.RepoName = repoName
+	} else {
+		info.RepoName = userData.Repo
+	}
+	repoData := (*userData.Data)[info.RepoName]
+	if repoData == nil {
+		return info
+	}
+	info.Repo = repoData.Repo
+	info.Log = repoData.Log
+	return info
+}
+
+func storeInfo(v *logCtlInfo) (err error) {
+	if v == nil {
+		return fmt.Errorf("待保存的参数 *logCtlInfo 为 nil")
+	}
+	ctxDataMap := _logCtlCtx.Data
+	if ctxDataMap == nil {
+		temp := make(map[string]*logCtlCtxData)
+		ctxDataMap = &temp
+	}
+
+	ctxData := (*ctxDataMap)[v.User]
+	if ctxData == nil {
+		ctxData = &logCtlCtxData{}
+	}
+	ctxData.AK = v.Ak
+	ctxData.SK = v.Sk
+	ctxData.Range = v.Range
+	ctxData.Repo = v.RepoName
+
+	ctxDataDataMap := ctxData.Data
+	if ctxDataDataMap == nil {
+		temp := make(map[string]*logCtlCtxRepoData)
+		ctxDataDataMap = &temp
+	}
+
+	repoData := &logCtlCtxRepoData{}
+	repoData.Repo = v.Repo
+	repoData.Log = v.Log
+
+	(*ctxDataDataMap)[v.RepoName] = repoData
+	ctxData.Data = ctxDataDataMap
+
+	(*ctxDataMap)[v.User] = ctxData
+	_logCtlCtx.Data = ctxDataMap
+
+	bytes, err := json.Marshal(_logCtlCtx)
 	if err == nil && bytes != nil {
 		err = ioutil.WriteFile(userHomeDir()+"/"+_tempFile, bytes, 0666)
 		if err != nil {
 			log.Println("Opps....", err)
 		}
 	}
+	return
 }
 
 func buildClient() (err error) {
